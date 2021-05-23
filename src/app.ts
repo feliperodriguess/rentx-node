802cd57762983db83b6b61e0513bfe194436f46c
Felipe Augusto <felipe.augusto.rodrigues1@gmail.com>
2021-05-23 11:45:51 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => response.json({ hello: 'Hey buddy! 🍺' }))
+
+export default app
