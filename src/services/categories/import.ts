411095c2986ab517ced1c583097f38c2b71b25ba
Felipe Augusto <felipe.augusto.rodrigues1@gmail.com>
2021-05-23 23:36:16 -0300

    fix: Fix async/await on ImportCategoriesService

--- src/services/categories/import.ts
+++ src/services/categories/import.ts
@@ -34,11 +34,11 @@ class ImportCategoryService {
 
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file)
-    categories.map((category) => {
+    categories.map(async (category) => {
       const { name, description } = category
-      const categoryExists = this.categoriesRepository.findByName(name)
+      const categoryExists = await this.categoriesRepository.findByName(name)
       if (!categoryExists) {
-        this.categoriesRepository.create({ name, description })
+        await this.categoriesRepository.create({ name, description })
       }
     })
   }
